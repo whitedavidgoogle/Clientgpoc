b2713483dd1d9a260f97f424c912fcef57c93fd7
whitedavidgoogle <whitedavid@google.com>
2017-04-05 10:59:53 -0400

    main cleanup

--- main.py
+++ main.py
@@ -2,11 +2,6 @@
 import textindex
 from google.cloud import datastore
 
-def random_with_N_digits(4):
-	from random import randint
-    range_start = 10**(n-1)
-    range_end = (10**n)-1
-    return randint(range_start, range_end)
 def search(lu):
 
 	index = textindex.Index()
@@ -16,24 +11,10 @@ def dslist(lu):
 	print "textindex Class constructor"
 	list.lookuplist(lu)
 	print "call lookuplist function"
-	client = datastore.Client()
-	print "created Client class constructor"
-	
-	print "for lookp to put dict in DS"
-
-class Datastoremodel(db.Model):
-	db.fn=StringProperty()
-	
-	entry = list.lookuplist(lu)
-		print "call dictionary"
-		for i in entry:
-			entry.put(i)
-key = Datastoremodel(key_name=random_with_N_digits)
-
-
 
 
 
 lu=raw_input('Enter Word,separate multiples with a comma: ')
-#search(lu)
+search(lu)
+print '---now the just the files---'
 dslist(lu)
